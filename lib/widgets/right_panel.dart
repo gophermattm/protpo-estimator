4f1925cc0b4012d7c1e5a7213db29980d09009b9
Matt Moore <matt.moore@turbogopher.ai>
2026-03-06 12:47:20 -0600

    Fix: add dart:async TimeoutException import to right_panel

--- lib/widgets/right_panel.dart
+++ lib/widgets/right_panel.dart
@@ -16,6 +16,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async' show TimeoutException;
 import 'dart:convert';
 import '../theme/app_theme.dart';
 import '../providers/estimator_providers.dart';
